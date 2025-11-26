cda54c5b488082b1bcec93f19a9c73df8aa79f5e
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-11-26 13:05:56 +0100

    Added WiFi and MQTT to the Arduino

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -1,14 +1,32 @@
 #include "Config.h"
+#include "WiFiConnection.h"
+#include "MQTT.h"
 #include "Radar.h"
+
+WiFiConnection wiFiConnection(WIFI_SSID, WIFI_PASSWORD);
+MQTT mqtt(MQTT_SERVER, MQTT_PORT);
 
 Radar radar(2, 3, 4);
 
+unsigned long last_sensor_read;
+
 void setup()
 {
-    radar.Init();
+  wiFiConnection.Connect();
+  mqtt.Connect();
+
+  radar.Init();
 }
 
 void loop()
 {
+  unsigned long runtime = millis();
+
+  if (runtime - last_sensor_read >= 200) {
     radar.Scan();
+    last_sensor_read = runtime;
+  }
+
+  wiFiConnection.EnsureConnectivity(runtime);
+  mqtt.EnsureConnectivity();
 }
